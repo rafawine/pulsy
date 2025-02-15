2ec12707f84173d15296c28da128026613caf28f
Rafael Barrientos García <rafiprogramando@gmail.com>
2025-02-14 20:47:26 -0600

    Prueba de variables de entorno #8

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -6,6 +6,18 @@ if ! go version &> /dev/null; then
   exit 1
 fi
 
+if [ -f "$HOME/workspace/envs/${JOB_NAME}_env.sh" ]; then
+  source "$HOME/workspace/envs/${JOB_NAME}_env.sh"
+
+  if [ ! $? -eq 0 ]; then  # Verifica el código de salida de source
+    echo "Error: Falló al cargar las variables de entorno."
+    return 1  # Código de error
+  fi
+else
+  echo "Error: Variables de entorno no encontradas."
+  exit 1 
+fi
+
 if ! go build -o ./tmp/$JOB_NAME ./cmd/server; then
   echo "Error: Falló la compilación de la aplicación."
   exit 1
