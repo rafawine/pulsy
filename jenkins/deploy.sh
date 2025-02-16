a2ebf692ff84c49a0643cb7864eff327a0468207
Rafael Barrientos García <rafiprogramando@gmail.com>
2025-02-16 12:28:52 -0600

    Se imprimira status del servicio despues de reiniciarlo

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Verifica si el servicio está activo
 STATUS=$(systemctl is-active $JOB_NAME)
 
-# Evalúa el estado del servicio
 if [[ "$STATUS" == "active" ]]; then
   if ! sudo systemctl restart "$JOB_NAME" > /dev/null 2>&1; then
     echo "Error: No se pudo reiniciar el servicio $JOB_NAME."
@@ -16,6 +14,11 @@ elif [[ "$STATUS" == "inactive" ]]; then
   fi
 else
   echo "Error: No se pudo determinar el estado del servicio $JOB_NAME."
+fi
+
+sudo systemctl status "$JOB_NAME"; then
+  echo "Error: No se pudo obtener el estatus del servicio $JOB_NAME."
+  return 1
 fi
 
 exit 0
